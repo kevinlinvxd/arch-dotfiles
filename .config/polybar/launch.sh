cabd7292e7b78d1ae46489b25dad64a00a578267
Kevin Lin <kevinlin.vxd@gmail.com>
2022-05-26 12:38:07 -0500

    Update SSID in bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,9 +3,7 @@
 # Terminate already running bar instances
 killall -q polybar
 
-## Wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar primary -c ~/.config/polybar/config.ini &
-# polybar music -c ~/.config/polybar/config.ini &
-# polybar secondary -c ~/.config/polybar/config.ini &
