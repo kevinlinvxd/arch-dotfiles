a52348d85c9870ab668d5bf8190c4b23cab1f91b
Kevin Lin <kevinlin.vxd@gmail.com>
2022-09-16 20:07:47 -0500

    Update config files for BSPWM - multi monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env sh
 
+EXTERNAL_MONITOR="HDMI-0"
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar primary -c ~/.config/polybar/config.ini &
+# polybar primary -c ~/.config/polybar/config.ini &
+
+if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
+  polybar --reload primary -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
+  polybar --reload primary_external -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-secondary.log 2>&1 200>&- &
+else
+  polybar --reload primary -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
+fi
